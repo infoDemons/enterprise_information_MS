1a123f2a3ba25980ca35f9cc317451ab279a5132
lixinyang <741162208@qq.com>
2019-11-26 01:09:58 +0800

    增加两个表

--- dbserver/src/main/resources/init.sql
+++ dbserver/src/main/resources/init.sql
@@ -22,6 +22,55 @@ values (3, 'talk is cheap', 'show me the code');
 
 
 
+drop table if exists `enterprise`;
+create table `enterprise`
+(
+    `enterprise_id`                    int primary key,
+    `unified_social_credit_identifier` text,
+    `enterprise_name`                  text,
+    `registered_capital`               text,
+    `paid_in_capital`                  text,
+    `business_scope`                   text,
+    `legal_representative`             text,
+    `industry`                         text,
+    `telephone_number`                 text,
+    `email`                            text,
+    `registered_address`               text,
+    `form_of_business_enterprise`      text,
+    `business_registration_number`     text,
+    `registration_authority`           text,
+    `business_status`                  text
+);
+
+
+drop table if exists `enterprise_information_change`;
+create table `enterprise_information_change`
+(
+    `enterprise_id`           int,
+    `enterprise_name`         text,
+    `information_change_date` text,
+    `information_change_type` text,
+    `information_before`      text,
+    `information_after`       text,
+    `create_date`             text,
+    foreign key (`enterprise_id`) references `enterprise` (`enterprise_id`)
+);
+
+
+load data infile '/Users/albert/DB/project/dataset/企业基础信息.csv'
+    into table dbserver.enterprise
+    fields terminated by ','
+    enclosed by '"'
+    lines terminated by '\r\n' ignore 1 lines;
+
+
+load data infile '/Users/albert/DB/project/dataset/企业变更信息.csv'
+    into table dbserver.enterprise_information_change
+    fields terminated by ','
+    enclosed by '"'
+    lines terminated by '\r\n' ignore 1 lines;
+
+
 drop table if exists `user`;
 create table `user`
 (
