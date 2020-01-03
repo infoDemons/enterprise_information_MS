88cee6aae79e5d9844fa6b3f500f6e9814e1e2f8
lixinyang <741162208@qq.com>
2020-01-04 05:30:13 +0800

    注册

---
+++ dbserver/src/main/java/com/demon/dbserver/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.demon.dbserver.controller;
+
+import com.demon.dbserver.common.RespBean;
+import com.demon.dbserver.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(value = "/signup")
+    public RespBean signup(String username, String password) {
+        int i = userService.addUser(username, password);
+        if (i == 1) {
+            return RespBean.ok("注册成功!");
+        } else if (i == -1) {
+            return RespBean.error("用户名重复，注册失败!");
+        }
+        return RespBean.error("注册失败!");
+    }
+}
